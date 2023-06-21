e06ed004c7f6eabbfb871f07668bcd52883d5411
Luis Armando Aguayo A <luisarmandoaguayoaceves@gmail.com>
2023-06-21 12:20:30 -0600

    Primer comit cap 45

---
+++ bases/app-v3.ts
@@ -0,0 +1,74 @@
+import { Script } from "vm";
+
+// Objetos
+type automovil = {
+  carroceria: string,
+  modelo: string,
+  antibalas: boolean,
+  pasajeros:number
+  disparar?: () => void
+}
+
+const batimovil: automovil = {
+  carroceria: "Negra",
+  modelo: "6x6",
+  antibalas: true,
+  pasajeros:4,
+};
+
+const bumblebee: automovil = {
+  carroceria: "Amarillo con negro",
+  modelo: "4x2",
+  antibalas: true,
+  pasajeros:4,
+  disparar(){ // El metodo disparar es opcional
+    console.log("Disparando");
+  }
+};
+
+// Villanos debe de ser un arreglo de objetos personalizados
+type villanoInfo = {
+  nombre: string,
+  edad?: Number,
+  mutante: Boolean
+}
+const villanos: villanoInfo[] = [{
+  nombre:"Lex Luthor",
+  edad: 54,
+  mutante:false
+},{
+  nombre: "Erik Magnus Lehnsherr",
+  edad: 49,
+  mutante: true
+},{
+  nombre: "James Logan",
+  edad: undefined,
+  mutante: true
+}];
+
+// Multiples tipos
+// cree dos tipos, uno para charles y otro para apocalipsis
+
+type datos = {
+  poder: string,
+  estatura: Number
+}
+type organizacion = {
+  lider: boolean,
+  miembros: string[]
+}
+const charles: datos  = {
+  poder:"psiquico",
+  estatura: 1.78
+};
+
+const apocalipsis: organizacion = {
+  lider:true,
+  miembros: ["Magneto","Tormenta","Psylocke","Angel"]
+}
+
+// Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+let mystique: organizacion | datos;
+
+mystique = charles;
+mystique = apocalipsis;
