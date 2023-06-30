f9005ced8ae8068bcc7f0f916d8a169aab21b790
Luis Armando Aguayo A <luisarmandoaguayoaceves@gmail.com>
2023-06-30 12:54:42 -0600

    API rest con express  cramos un endPoint para hacer consultas de manera local

---
+++ express-API/index.ts
@@ -0,0 +1,21 @@
+import express  from "express"
+
+// const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.status(401).json({
+    ok: true,
+    msg: 'Error de validacion'
+  })
+  res.json({
+        ok: true,
+        msg: 'Formato JSON este es un END POINT es una api para consumir de manera local'
+    })
+//   res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}/`)
+})
